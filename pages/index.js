f8b6e79e5748c4116bf45c2ad383e5abbfd64bdd
SethHigh <sah6406@psu.edu>
2025-02-25 09:41:58 -0500

    transfered content from old version to new one

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,36 @@ import Hero from "@/components/LandingPage/Hero"
 import { styled } from 'styled-components'
 import Navbar from "@/components/Dashboard/Navbar"
 import Footer from "@/components/LandingPage/Footer"
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+
 export default function Home() {
   return (
     <>
-        <Navbar/>
-        <Hero text={'WELCOME TO MY CLASS'} />
-        <Hero />
-        <Footer />
+      <Head>
+        <title>Gif Collector</title>
+        <meta name="description" content="Find Yourself Some Gifs" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+      <header className={styles.banner}>
+        <div className={styles.navbar}>
+          <Link href="/favorites">
+                <button className={styles.button}>Favorites</button>
+          </Link>
+          <Link href="/login">
+                <button className={styles.button}>Login</button>
+          </Link>
+          <h1>Search Gifs</h1>
+          <input type="text" id="searchBox" placeholder="Search for GIFs"></input>
+          <button id="searchButton" className={styles.button}>Search</button>
+          <div id="gifContainer"></div>
+        </div>
+      </header>
+      </div>
     </>
   )
 }

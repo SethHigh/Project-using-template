40a633af47660c916e6f0368fed220b7fe033cc3
SethHigh <sah6406@psu.edu>
2025-02-27 04:06:18 -0500

    made main page show different buttons if logged in or logged out

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,45 @@ import Navbar from "@/components/Dashboard/Navbar"
 import Footer from "@/components/LandingPage/Footer"
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { useStateContext } from '@/context/StateContext'
+import { auth } from '@/backend/Firebase'
+import { signOut } from 'firebase/auth'
 
 
 export default function Home() {
+  const { user } = useStateContext()
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth)
+      console.log('User signed out')
+    } catch (error) {
+      console.error('Error signing out:', error)
+    }
+  }
+
   return (
     <>
       <div>
       <header className={styles.banner}>
         <div className={styles.navbar}>
-          <Link href="auth/signup">
+        {user ? (
+              <>
+                <Link href="/favorites">
+                  <button className={styles.button}>Favorites</button>
+                </Link>
+                <button onClick={handleSignOut} className={styles.button}>Sign Out</button>
+              </>
+            ) : (
+              <>
+              <Link href="auth/signup">
                 <button className={styles.button}>sign up</button>
-          </Link>
-          <Link href="auth/login">
+              </Link>
+              <Link href="auth/login">
                 <button className={styles.button}>Login</button>
-          </Link>
+              </Link>
+            </>
+          )}
           <h1>Search Gifs</h1>
           <input type="text" id="searchBox" placeholder="Search for GIFs"></input>
           <button id="searchButton" className={styles.button}>Search</button>
